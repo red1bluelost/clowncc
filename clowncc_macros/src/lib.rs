6f856d9918671e5ca3eaaa0aa4488ae54275f51a
Micah Weston <micahsweston@gmail.com>
2023-11-06 22:38:29 -0500

    Migrates to using external context_type on github.

--- clowncc_macros/src/lib.rs
+++ clowncc_macros/src/lib.rs
@@ -1,6 +1,1 @@
-pub mod context_bool;
 pub mod debug_assertions;
-
-pub mod __internal {
-    pub use paste as __paste;
-}
--- clowncc_token/src/cursor.rs
+++ clowncc_token/src/cursor.rs
@@ -34,18 +34,18 @@ impl QuoteType {
     }
 }
 
-clowncc_macros::yes_no! {
+context_type::boolean::yes_no! {
     /// When parsing a universal character or escaped newline, the caller may
     /// want the initial backslash consumed or not consumed during the
     /// speculative parse.
     enum EatSlash;
 }
-clowncc_macros::yes_no! {
+context_type::boolean::yes_no! {
     /// Tokenizing has no state so the caller must choose when to treat quotes
     /// `"` or angle brackets `<` as headers or regular tokens.
     enum ExpectHeader;
 }
-clowncc_macros::yes_no! {
+context_type::boolean::yes_no! {
     /// Parsing an identifier may perform a few steps before entering the
     /// dedicated function. If a universal char is consumed, it must be
     /// communicated to the function as it consumes the rest of the identifier.
